de067827e7c3d8b5adf4ff87b55cda3f7db872f8
yash012 <yash.singhal_cs19@gla.ac.in>
2020-08-25 00:51:53 -0700

    initializing the fields of Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -7,4 +7,17 @@
 package definitions;
 
 public class Book {
+    // Components of a Definition Class:
+    // 1. private fields.
+    // 2. Getter methods for private fields.
+    // 3. Setter methods for private fields.
+    // 4. Constructor methods for initializing the attributes of the object.
+    // 5. The toString() method.
+    // 6. The equals() and the hashCode() method
+
+    private String bookName;
+    private String authorName;
+    private String iSBNumber;
+
+
 }
