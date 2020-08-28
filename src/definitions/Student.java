21302da283f2bc4545ef42c0f60b597c517c8b94
yash012 <yash.singhal_cs19@gla.ac.in>
2020-08-28 05:24:22 -0700

    create the non-parameterized constructor of the fields

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -13,4 +13,25 @@ public class Student {
     private long universityRollNumber;
     private int numberOfBookIssued;
     private Book[] store;
+
+    // The Constructor Method : Used to initialize the attributes of an object.
+    // public <non-static> <no-return type> <SameAsTheClassName> () {}
+
+    // The Java compiler creates a no-parameter constructor for us automatically.
+    // default constructor
+
+    // we can create two types of constructors:
+    // 1. Non-Parameterized Constructors : The constructor has no parameters.
+    public Student() {
+        this.firstName = "";
+        this.middleName = "";
+        this.lastName = "";
+        this.universityRollNumber = 0;
+        this.numberOfBookIssued = 0;
+
+    }
+}
+
+
+
 }
