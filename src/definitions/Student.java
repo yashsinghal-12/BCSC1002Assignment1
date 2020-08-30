82717aade0273e45c3120738d5bb9c293b5aa88a
yash012 <yash.singhal_cs19@gla.ac.in>
2020-08-30 03:59:05 -0700

    add a @override annotation method in student class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Student {
     private String firstName;
     private String middleName;
@@ -97,6 +99,27 @@ public class Student {
                 "First Name: %s/n, Middle Name: %s/n, last Name: %s, University Roll Number: %d, Number Of Book Issued: %d, Name of the Books issued by Student: %s",
                 getFirstName(), getMiddleName(), getLastName(), getUniversityRollNumber(), getNumberOfBookIssued(), Arrays.toString(getStore())
         );
+    }
+    // The equals() and the hashCode() methods.
+    // The Object class in Java is the super class (parent) of all other classes.
+    // When we create any class in Java, it automatically inherits the Object class.
+
+    // book.equals(book1)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { // book == book1
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        Student book = (Student) o;
+        return this.getFirstName().equals(book.getFirstName()) &&
+                this.getMiddleName().equals(book.getMiddleName()) &&
+                this.getLastName().equals(book.getLastName()) &&
+                this.getUniversityRollNumber() == book.getUniversityRollNumber() &&
+                this.getNumberOfBookIssued() == book.getNumberOfBookIssued() &&
+                Arrays.equals(getStore(), book.getStore());
     }
 
 }
